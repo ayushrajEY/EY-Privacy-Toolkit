d8c60528062cf4e171b4125cb6b5d6f7be48c042
gauravsharan09 <129146875+gauravsharan09@users.noreply.github.com>
2023-03-30 22:41:49 +0530

    Tried adding Question card

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
 import React, { useState } from 'react';
 import './App.css';
+import QuizComp from './QuestionComponent/QuizComp';
 
 function App() {
 
   const [isFlipped, setIsFlipped] = useState(false);
 
   const handleFlip = () => {
-    setIsFlipped(!isFlipped);
+    setIsFlipped(true);
   };
 
   return (
     <div className="container">
-      <div className= "background-image">
-        <img src="bg.png"/>
-      </div>
-      <div className="logo">
-        <img src="logo.png" alt="React Logo" className="logo" />
-      </div>
-      <div className={`paper ${isFlipped ? 'flipped' : ''}`} align = "center">
-        <h1>WELCOME !</h1>
-        <br></br>
-        <p>The Questionaire will take 10 minutes to complete</p>
-        <br></br>
-        <h4>Click on the button to Continue</h4>
-      </div>
-      <div className="background-image">
-      </div>
-      <button className="rounded" onClick={handleFlip}>Continue</button>
+      {isFlipped ? (
+        <QuizComp />
+      ) : (
+        <>
+          <div className="background-image">
+            <img src="bg.png" />
+          </div>
+          <div className="logo">
+            <img src="logo.png" alt="React Logo" className="logo" />
+          </div>
+          <div className={`paper ${isFlipped ? 'flipped' : ''}`} align="center">
+            <h1>WELCOME !</h1>
+            <br />
+            <p>The Questionaire will take 10 minutes to complete</p>
+            <br />
+            <h4>Click on the button to Continue</h4>
+          </div>
+          <div className="background-image"></div>
+          <button className="rounded small-button" onClick={handleFlip}>Continue</button>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/QuestionComponent/QuizComp.js
@@ -0,0 +1,119 @@
+import React from 'react'
+import { useState } from 'react';
+import './quizstyle.css'
+
+
+const QuizComp = () => {
+    var Questionbank = [
+        {
+            Question: "What is the name of your Organization?",
+            Answers: [
+                { Answer: "Adobe", isCorrect: true },
+                { Answer: "Microsoft", isCorrect: false },
+                { Answer: "Google", isCorrect: false },
+                { Answer: "TCS", isCorrect: false }
+            ]
+        },
+        {
+            Question: "What is the region of your Organization?",
+            Answers: [
+                { Answer: "Asia", isCorrect: false },
+                { Answer: "Africa", isCorrect: true },
+                { Answer: "Europe", isCorrect: false },
+                { Answer: "Australia", isCorrect: false }
+            ]
+        }, {
+            Question: "2 +3 = ?",
+            Answers: [
+                { Answer: "5", isCorrect: true },
+                { Answer: "7", isCorrect: false },
+                { Answer: "4", isCorrect: false },
+                { Answer: "3", isCorrect: false }
+            ]
+        },
+        {
+            Question: "What comes after january?",
+            Answers: [
+                { Answer: "feb", isCorrect: true },
+                { Answer: "march", isCorrect: false },
+                { Answer: "june", isCorrect: false },
+                { Answer: "sept", isCorrect: false }
+            ]
+        },
+        {
+            Question: "What kind of organisation is yours?",
+            Answers: [
+                { Answer: "Consulting", isCorrect: false },
+                { Answer: "Development", isCorrect: false },
+                { Answer: "Service Prov", isCorrect: true },
+                { Answer: "Product Based", isCorrect: false }
+            ]
+        }
+    ]
+
+    //useState Hook
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [score, setScore] = useState(0);
+    const [showScore, setShowScore] = useState(false);
+
+const handleAnswerResponse=(isCorrect)=>
+{
+    if(isCorrect)
+    {
+        setScore(score+1);
+    }
+
+   const nextQuestion= currentQuestion+1;
+   if(nextQuestion<Questionbank.length)
+   {
+    setCurrentQuestion(nextQuestion);
+   }
+   else{
+    setShowScore(true);
+   }
+}
+
+const resetQuiz=()=>
+{
+    setCurrentQuestion(0);
+    setScore(0);
+    setShowScore(false);
+}
+
+    return (
+        <div className='app'>
+            {showScore ? (
+                <div className='score-section'>
+                    You have scored {score} out of {Questionbank.length}
+                    <>
+                       <button type="submit" onClick={resetQuiz}>Play Again!!</button>
+                    </>
+                </div>
+            )
+                : (
+                    <>
+                        <div className='question-section'>
+                            <div className='question-count'>
+                               <span>{currentQuestion+1}</span>/{Questionbank.length}
+                            </div>
+
+                            <div className='question-text'>
+                             {Questionbank[currentQuestion].Question}
+                            </div>
+                        </div>
+
+                        <div className='answer-section'>
+                          {Questionbank[currentQuestion].Answers.map((answer)=>
+                          (
+                              <button onClick={()=>handleAnswerResponse(answer.isCorrect)}>{answer.Answer}</button>
+                          ))}
+                        </div>
+                    </>
+                )
+            }
+
+        </div>
+    );
+}
+
+export default QuizComp;
